ad88420d092505e9e196841ae46490a6bdc46508
MUSTAFA ALKAN <250983@ptt.local>
2019-04-16 16:48:33 +0300

    18.11 bitti

---
+++ src/store/actions/auth.js
@@ -0,0 +1,62 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (idToken, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: idToken,
+        userId: userId
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000)
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        const apikey = 'AIzaSyDxAgsHBmNBVT7YmakfpWN0oH1leqyJzRM';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + apikey;
+        if (isSignup) {
+            url= 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + apikey;
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                dispatch(authFail(err.response.data.error));
+            });
+    };
+};

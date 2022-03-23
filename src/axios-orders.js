1f7f825ea7b57f1e00fadc157394df4cec55621f
Mustafa Alkan <53699482+mususi@users.noreply.github.com>
2022-03-23 20:04:50 +0300

    Update axios-orders.js

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger-84b2d.firebaseio.com/'
+    baseURL: 'https://react-burger-builder-4b067.firebaseio.com/'
 });
 
 export default instance;
